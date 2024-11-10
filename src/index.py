9a55756140d7aafee7351541b1e748057a48ec9d
honjen <144600573+honjen@users.noreply.github.com>
2024-11-10 14:01:26 +0200

    Add pylint to varasto

--- src/index.py
+++ src/index.py
@@ -1,27 +1,28 @@
 from varasto import Varasto
 
-
-def main():
-    mehua = Varasto(100.0)
-    olutta = Varasto(100.0, 20.2)
-
+def alkutilanne(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
+def olut_getterit(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+def mehu_paivitys_lisaa(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
     print(f"Mehuvarasto: {mehua}")
+
+def mehu_paivitys_ota(mehua):
     print("Otetaan 3.14")
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+def virhe():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,28 +32,45 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+def olut_paivitys_lisaa(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
     print(f"Olutvarasto: {olutta}")
 
+def mehu_paivitys_lisaa_virhe(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.lisaa_varastoon(-666.0)")
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+def olut_paivitys_ota(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+def mehu_paivitys_otto_virhe(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
 
+def main():
+    mehua = Varasto(100.0)
+    olutta = Varasto(100.0, 20.2)
+
+    alkutilanne(mehua, olutta)
+    olut_getterit(olutta)
+    mehu_paivitys_lisaa(mehua)
+    mehu_paivitys_ota(mehua)
+    virhe()
+    olut_paivitys_lisaa(olutta)
+    mehu_paivitys_lisaa_virhe(mehua)
+    olut_paivitys_ota(olutta)
+    mehu_paivitys_otto_virhe(mehua)
 
 if __name__ == "__main__":
     main()
--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,41 +38,41 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
-    
+
     # uudet testit alla
 
     def test_lisays_yli_tilavuuden(self):
         # varastoon yritetään laittaa tavaraa enemmän kuin on tilaa
         self.varasto.lisaa_varastoon(13)
         self.assertAlmostEqual(self.varasto.saldo, 10)
-    
+
     def test_ottaminen_yli_saldon(self):
         # yritetään ottaa enemmän kuin varastossa on tavaraa
         self.varasto.lisaa_varastoon(5)
         saatu_maara = self.varasto.ota_varastosta(7)
         self.assertAlmostEqual(saatu_maara, 5)
         self.assertAlmostEqual(self.varasto.saldo, 0)
-    
+
     def test_konstruktori_negatiivinen_tilavuus(self):
         # annetaan varastolle negatiivinen tilavuus
         varasto = Varasto(-2)
         self.assertAlmostEqual(varasto.tilavuus, 0)
-    
+
     def test_konstruktori_negatiivinen_alku_saldo(self):
         # annetaan varastolle negatiivinen alkusaldo
         varasto = Varasto(10, -3)
         self.assertAlmostEqual(varasto.saldo, 0)
-    
+
     def test_lisays_negatiivinen_maara(self):
         # lisätään negatiivinen määrä
         self.varasto.lisaa_varastoon(-4)
         self.assertAlmostEqual(self.varasto.saldo, 0)
-    
+
     def test_ottaminen_negatiivinen_maara(self):
         # yritetään ottaa negatiivinen määrä varastosta
         saatu_maara = self.varasto.ota_varastosta(-2)
         self.assertAlmostEqual(saatu_maara, 0)
-    
+
     def test_varaston_tulostus(self):
         # onko tulostus oikein?
         self.varasto.lisaa_varastoon(4)
